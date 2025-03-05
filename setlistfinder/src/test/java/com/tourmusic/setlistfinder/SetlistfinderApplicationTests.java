2faede544ee6a282321fe2c9e2f5b83f4ba577e1
Geoffrey87 <lloydcosta@hotmail.com>
2025-03-05 15:53:20 +0000

    add setlistfinder spring dependencies

---
+++ setlistfinder/src/test/java/com/tourmusic/setlistfinder/SetlistfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourmusic.setlistfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetlistfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
