8b834f27bbb993aee5d69f6ddd12cc82688a7d12
Geoffrey87 <lloydcosta@hotmail.com>
2025-04-07 17:28:14 +0100

    delete unecessary files

--- setlistfinder/src/main/java/com/tourmusic/setlistfinder/repo/SetlistRepo.java
+++
@@ -1,9 +0,0 @@
-package com.tourmusic.setlistfinder.repo;
-
-import com.tourmusic.setlistfinder.entitys.Setlist;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SetlistRepo extends JpaRepository<Setlist, Long> {
-}
--- setlistfinder/src/main/java/com/tourmusic/setlistfinder/service/IBandService.java
+++
@@ -1,7 +0,0 @@
-package com.tourmusic.setlistfinder.service;
-
-import com.tourmusic.setlistfinder.dto.BandDTO;
-
-public interface IBandService {
-    BandDTO getBandByName(String name);
-}
