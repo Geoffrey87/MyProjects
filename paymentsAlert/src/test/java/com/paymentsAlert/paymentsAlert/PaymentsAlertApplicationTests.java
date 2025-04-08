0ca9a3fa5515538342578117f20df6856fee5b4c
Geoffrey87 <lloydcosta@hotmail.com>
2025-04-08 12:21:30 +0100

    create folder with spring paymentAlert

---
+++ paymentsAlert/src/test/java/com/paymentsAlert/paymentsAlert/PaymentsAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentsAlert.paymentsAlert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
