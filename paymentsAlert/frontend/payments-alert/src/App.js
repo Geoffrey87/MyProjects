1942c4774b57a34e6ae8d67047a86924c88b8269
Geoffrey87 <lloydcosta@hotmail.com>
2025-04-10 15:13:01 +0100

    start the front-end with react

---
+++ paymentsAlert/frontend/payments-alert/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+          <Routes>
+
+          </Routes>
+        </Router>
+  );
+}
+
+export default App;
